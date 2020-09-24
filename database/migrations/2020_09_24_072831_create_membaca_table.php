83d1d67173239c945d2fb30bba1abf42ae72d799
bungananda27 <bungatiani@gmail.com>
2020-09-24 14:33:07 +0700

    create table warna, kritik, membaca

---
+++ database/migrations/2020_09_24_072831_create_membaca_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMembacaTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('membaca', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('gambar');
+            $table->string('sound_id');
+            $table->string('tulisan_id');
+            $table->string('sound_en');
+            $table->string('tulisan_en');
+            $table->tinyInteger('tipe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('membaca');
+    }
+}
