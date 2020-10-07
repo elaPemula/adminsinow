be1b41ffb0bfa0b20058c0cd78155e50ecf49661
Agasi Gilang Persada <464516p@gmail.com>
2020-10-07 16:20:35 +0700

    Tambah api mewarnai

---
+++ app/Http/Controllers/Api/MewarnaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Mewarna;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MewarnaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return response()->json([
+            'data' => Mewarna::all(),
+            'message' => 'Sukses ambil data',
+        ]);
+    }
+
+    public function show(Mewarna $mewarna)
+    {
+        return response()->json([
+            'data' => $mewarna,
+            'message' => 'Sukses ambil data',
+        ]);
+    }
+}
