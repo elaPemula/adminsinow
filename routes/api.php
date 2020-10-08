627c84e8bf3cccc887ed55595922744efeb0d46f
Agasi Gilang Persada <464516p@gmail.com>
2020-10-08 14:55:22 +0700

    Ganti nama route di api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,32 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('mewarna', 'Api\MewarnaController');
-Route::apiResource('membaca', 'Api\MembacaController');
-Route::apiResource('warna', 'Api\WarnaController');
-Route::apiResource('menyanyi', 'Api\MenyanyiController');
-Route::apiResource('huruf', 'Api\HurufController');
-Route::apiResource('angka', 'Api\AngkaController');
-Route::apiResource('quiz', 'Api\QuizController');
+Route::apiResource('mewarna', 'Api\MewarnaController')->names([
+    'index' => 'mewarna-api.index',
+    'show' => 'mewarna-api.show',
+])->only(['index', 'show']);
+Route::apiResource('membaca', 'Api\MembacaController')->names([
+    'index' => 'membaca-api.index',
+    'show' => 'membaca-api.show',
+])->only(['index', 'show']);
+Route::apiResource('warna', 'Api\WarnaController')->names([
+    'index' => 'warna-api.index',
+    'show' => 'warna-api.show',
+])->only(['index', 'show']);
+Route::apiResource('menyanyi', 'Api\MenyanyiController')->names([
+    'index' => 'menyanyi-api.index',
+    'show' => 'menyanyi-api.show',
+])->only(['index', 'show']);
+Route::apiResource('huruf', 'Api\HurufController')->names([
+    'index' => 'huruf-api.index',
+    'show' => 'huruf-api.show',
+])->only(['index', 'show']);
+Route::apiResource('angka', 'Api\AngkaController')->names([
+    'index' => 'angka-api.index',
+    'show' => 'angka-api.show',
+])->only(['index', 'show']);
+Route::apiResource('quiz', 'Api\QuizController')->names([
+    'index' => 'quiz-api.index',
+    'show' => 'quiz-api.show',
+])->only(['index', 'show']);
