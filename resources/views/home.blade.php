a29d4ddb529140fcfc6a8e120535b0e88af03145
bungananda27 <bungatiani@gmail.com>
2020-11-17 15:53:51 +0700

    VIEW dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,227 +1,202 @@
 @extends('master')
 @section('content')
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Belajar</h2>
-                <div class="main-income-phara">
-                    <p>angka</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
+<div class="data-table-area mg-b-15">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Belajar</h2>
+                                <div class="main-income-phara">
+                                    <p>angka</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
                     </div>
                 </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Belajar</h2>
+                                <div class="main-income-phara">
+                                    <p>huruf</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Belajar</h2>
+                                <div class="main-income-phara">
+                                    <p>warna</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Belajar</h2>
+                                <div class="main-income-phara">
+                                    <p>buah/hewan</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Quiz</h2>
+                                <div class="main-income-phara">
+                                    <p>hitung/baca</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Hiburan</h2>
+                                <div class="main-income-phara">
+                                    <p>menyanyi</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Hiburan</h2>
+                                <div class="main-income-phara">
+                                    <p>mewarna</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="income-dashone-total shadow-reset nt-mg-b-30">
+                        <div class="income-title">
+                            <div class="main-income-head">
+                                <h2>Kritik dan Saran</h2>
+
+                            </div>
+                        </div>
+                        <div class="income-dashone-pro">
+                            <div class="income-rate-total">
+                                <div class="price-adminpro-rate">
+                                    <h3> <span class="counter">27</span></h3>
+                                </div>
+                            </div>
+                            <div class="income-range">
+                                <p>Jumlah diakses</p>
+                            </div>
+                            <div class="clear"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="clear"></div>
         </div>
     </div>
 </div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Belajar</h2>
-                <div class="main-income-phara">
-                    <p>huruf</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Belajar</h2>
-                <div class="main-income-phara">
-                    <p>warna</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Belajar</h2>
-                <div class="main-income-phara">
-                    <p>buah dan hewan</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Quiz</h2>
-                <div class="main-income-phara">
-                    <p>hitung</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Quiz</h2>
-                <div class="main-income-phara">
-                    <p>hitung</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Hiburan</h2>
-                <div class="main-income-phara">
-                    <p>menyanyi</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
-<div class="col-lg-3">
-    <div class="income-dashone-total shadow-reset nt-mg-b-30">
-        <div class="income-title">
-            <div class="main-income-head">
-                <h2>Hiburan</h2>
-                <div class="main-income-phara">
-                    <p>mewarna</p>
-                </div>
-            </div>
-        </div>
-        <div class="income-dashone-pro">
-            <div class="income-rate-total">
-                <div class="price-adminpro-rate">
-                    <h3><span>$</span><span class="counter">60888200</span></h3>
-                </div>
-                <div class="price-graph">
-                    <span id="sparkline1"><canvas width="27" height="19"
-                            style="display: inline-block; width: 27px; height: 19px; vertical-align: top;"></canvas></span>
-                </div>
-            </div>
-            <div class="income-range">
-                <p>Total income</p>
-                <span class="income-percentange">98% <i class="fa fa-bolt"></i></span>
-            </div>
-            <div class="clear"></div>
-        </div>
-    </div>
-</div>
+
+
+
+
+
+
 @endsection
